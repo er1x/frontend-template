3e6d6f1ab3d8c00a0210b12e3ad61f160e5d5451
Emilio Grande <erixcbr@gmail.com>
2015-03-09 20:52:56 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# frontend-template
+
+A frontend project scaffold including:
+
+  - Stylus with nib
+  - Browserify
+  - Gulp tasks
+
+## gulp tasks
+
+```bash
+gulp  # serve on localhost:9000
+gulp build # optimize and deploy on ./dist folder
+```
---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+var gulp       = require('gulp'),
+    browserify = require('gulp-browserify'),
+    stripDebug = require('gulp-strip-debug'),
+    connect    = require('gulp-connect'),
+    rename     = require('gulp-rename'),
+    stylus     = require('gulp-stylus'),
+    minifyHTML = require('gulp-minify-html'),
+    nib        = require('nib'),
+    uglify     = require('gulp-uglify');
+
+
+/// Subtasks
+/// 
+/// 
+
+// JS
+gulp.task('js-dev', function () {
+  gulp.src('app/js/main.js')
+    .pipe(browserify())
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('./app/js'));
+});
+
+gulp.task('js', function () {
+  gulp.src('app/js/main.js')
+    .pipe(browserify())
+    .pipe(uglify({ compress: true }))
+    .pipe(stripDebug())
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
+/// /JS
+
+
+/// Stylus
+gulp.task('stylus-dev', function () {
+  gulp.src('app/stylus/main.styl')
+    .pipe(stylus({use: nib()}))
+    .pipe(rename('bundle.css'))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('stylus', function () {
+  gulp.src('app/stylus/main.styl')
+    .pipe(stylus({compress: true, use: nib()}))
+    .pipe(rename('bundle.css'))
+    .pipe(gulp.dest('./dist/css'));
+});
+/// /Stylus
+
+
+/// HTML
+gulp.task('html-dev', function () {
+  gulp.src('./app/**/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+  .pipe(minifyHTML())
+  .pipe(gulp.dest('./dist'));
+});
+/// /HTML
+
+
+/// Misc
+gulp.task('fonts', function () {
+  gulp.src('app/fonts/**')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+/// /Misc
+
+
+// Connect & Watch
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port: 9000
+  });
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./app/**/*.html'], ['html-dev']);
+  gulp.watch(['./app/js/**/*'], ['js-dev']);
+  gulp.watch(['./app/stylus/**/*'], ['stylus-dev']);
+});
+
+///
+/// /Subtasks 
+
+
+
+// Main tasks
+
+// Serve task
+gulp.task('default', ['connect', 'js-dev', 'stylus-dev', 'watch']);
+
+// Optimize and build task
+gulp.task('build', ['js', 'stylus', 'html', 'fonts']);
